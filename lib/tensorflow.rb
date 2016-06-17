0f785968e9bf0142425799555968cc0f478fae3d
Arafatk <arafat@iitkgp.ac.in>
2016-06-17 18:42:38 +0530

    Correcting type checking for the data

--- lib/tensorflow.rb
+++ lib/tensorflow.rb
@@ -23,25 +23,27 @@ class Tensor
   end
   
   def type_check(data)
-     start = data[0]
-     while (start.is_a?(Array))
-     	start = start[0]
-     end
+  	data_flatten = data.flatten
+    start = data_flatten[0]
      # Take care of boolean and complex numbers too
-     if start.is_a? Integer
-     	type = Integer
-     elsif start.is_a? Float
-     	type = Float
-     elsif start.is_a? String
-        type = String
-     else 
-     	raise "Data type not supported."
-     end
-     data.each do |i|
-     #	raise "Different data types in array." if !(i.is_a?  (type))
-     end
-     type
+    if start.is_a? Integer
+      type = Integer
+    elsif start.is_a? Float
+      type = Float
+    elsif start.is_a? String
+      type = String
+    else 
+      raise "Data type not supported."
+    end
+    data_flatten.each do |i|
+      raise "Different data types in array." if !(i.is_a?  (type))
+    end
+    type
   end
 end
-a = Tensor.new([[[2,3,4,5,6,8],[2,3,4,5,6,8]],[[2,3,4,5,6,8],[2,3,4,5,6,8]],[[2,3,4,5,6,8],[2,3,4,5,6,8]],[[2,3,4,5,6,8],[2,3,4,5,6,8]]])
-print a.dimensions,"\n",a.rank,"\n"
+# example to be removed in the next commit 
+a = Tensor.new([[[[2.4,3.2,4.4,5.5,6.2,8.0],[2.0,3.4,4.5,5.1,6.2,8.4]],[[2.4,3.2,4.4,5.5,6.2,8.0],[2.0,3.4,4.5,5.1,6.2,8.4]]],[[[2.4,3.2,4.4,5.5,6.2,8.0],[2.0,3.4,4.5,5.1,6.2,8.4]],[[2.4,3.2,4.4,5.5,6.2,8.0],[2.0,3.4,4.5,5.1,6.2,8.4]]],[[[2.4,3.2,4.4,5.5,6.2,8.0],[2.0,3.4,4.5,5.1,6.2,8.4]],[[2.4,3.2,4.4,5.5,6.2,8.0],[2.0,3.4,4.5,5.1,6.2,8.4]]],[[[2.4,3.2,4.4,5.5,6.2,8.0],[2.0,3.4,4.5,5.1,6.2,8.4]],[[2.4,3.2,4.4,5.5,6.2,8.0],[2.0,3.4,4.5,5.1,6.2,8.4]]],[[[2.4,3.2,4.4,5.5,6.2,8.0],[2.0,3.4,4.5,5.1,6.2,8.4]],[[2.4,3.2,4.4,5.5,6.2,8.0],[2.0,3.4,4.5,5.1,6.2,8.4]]]])
+print a.dimensions,"\n",a.type,"\n",a.rank,"\n"
+# [5, 2, 2, 6]
+# Float
+# 4
