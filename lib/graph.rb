df6cd3282b7ad29814320031b8f756d711f45940
Arafatk <arafat@iitkgp.ac.in>
2016-06-19 12:31:10 +0530

    Making chnges to graph

--- lib/graph.rb
+++ lib/graph.rb
@@ -1,11 +1,13 @@
 require_relative 'tensorflow'
 
 class GraphNode
-  attr_accessor :node_def
+  attr_accessor :def, :ref, :outDataTypes
+  def initialize
+  end
 end
 
 class Graph
-  attr_accessor :availableOps, :constants, :variables, :graph_def
+  attr_accessor :availableOps, :constants, :variables, :graph_def, :op, :placeholder
   def initialize()
   	self.availableOps = loadAvailableOps()
   end
@@ -24,16 +26,50 @@ class Graph
   	reader = File.read(filename)
   	self.graph_def = Tensorflow::GraphDef.decode(reader)
   end
+
+
+
+  def placeholder(name, type_enum, dims)
+    op = GraphNode.new()
+    op.def = Tensorflow::NodeDef.new(:name => name,:op=> "Placeholder", :attr => Hash.new)
+    op.outDataTypes = Hash.new
+    op.def.attr["dtype"] =  Tensorflow::AttrValue.new(:type => type_enum)
+    dim_array = []
+    dims.each do |i|
+      dim_array.push(Tensorflow::TensorShapeProto::Dim.new(:size => i))
+    end
+    op.def.attr["shape"] = Tensorflow::AttrValue.new(:shape => Tensorflow::TensorShapeProto.new(:dim => dim_array))
+    op
+  end
+
+
+  def op_definer(opName, name , input, device, attrs)
+    op = self.availableOps[opName.downcase!]
+    raise ("Operation not found.") if !op 
+    raise ("Invalid number of inputs.") if op.input_arg.length != input.length
+    inputs = []
+    input.each do |node|
+       inputs.push(node.def.name)
+    end
+    node = GraphNode.new()
+    node.def = Tensorflow::NodeDef.new(:name => name, :op => opName, :input => inputs, :device => device , :attr => Hash.new)
+    node.outDataTypes = Hash.new
+    attrs = Hash.new if attrs == nil
+    matchTypes(input, node, attrs, op)
+  end
+  
+  def matchTypes(input, outnode, attrs, op)
+    (0..op.input_arg.length - 1).each do |i|
+      inType = input[i].outDataTypes[input[i].def.name]
+      #if inTypeDefined && inType != DTInvalid && arg.TypeAttr != "" { attrs[arg.TypeAttr] = inType }
+    end
+  end
 end
 
-def placeholder()
 
-
-end
 a = Graph.new()
-puts a.availableOps["addn"].input_arg[0]
-puts a.availableOps["addn"]
 
-availableOps = Hash.new
-availableOps["rwewer"] = Tensorflow::AttrValue.new
-b = Tensorflow::NodeDef.new(:name => "wqeq",:op=>"rd", :attr => availableOps)
+d = Graph.new()
+inpt = [d.placeholder("input1", 4, [3])]
+inpt.push(d.placeholder("input2", 4, [3]))
+a.op_definer("Add","Output", inpt, "", nil)
