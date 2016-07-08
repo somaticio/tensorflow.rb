eac9527cf471bc514710a7169cf0be6b041a73c9
nethsix <nethsix@gmail.com>
2016-07-08 14:42:02 +0900

    With rvm the require 'tf/Tensorflow' should consider GEM_HOME
    If rvm is used to install ruby-tensorflow, then it's location
    is not in global search path thus we need to require the
    Tensorflow.bundle using a path based on GEM_HOME.

--- lib/tensorflow.rb
+++ lib/tensorflow.rb
@@ -1,6 +1,10 @@
 require 'tensorflow/core/framework/tensor'
 require 'tensorflow/core/framework/graph'
-require 'tf/Tensorflow'
+if ENV['rvm_version'].nil?
+  require 'tf/Tensorflow'
+else
+  require "#{ENV['GEM_HOME']}/gems/tensorflow-0.0.1/lib/tf/Tensorflow"
+end
 require 'tensor'
 require 'graph'
 require 'session'
