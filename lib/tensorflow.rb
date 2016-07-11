c5bdab270fd87b87acb2ef093f6975cab2828b83
nethsix <nethsix@gmail.com>
2016-07-11 23:37:49 +0900

    Restructure Gem 'lib/' to conform to best practices
    To avoid Graph, Session, etc. from occupying the global
    namespace, we restructure them to be prefixed with Tensorflow::.
    
    * Move lib/graph.rb, lib/ops.pb, lib/session.rb, lib/tensor.rb
      into lib/tensorflow/
    * We prefix Graph, Session, Tensor with Tensorflow::
    * We prefix the 'require' os graph, session, and tensor in
      lib/tensorflow.rb with 'tensorflow/'

--- lib/tensorflow.rb
+++ lib/tensorflow.rb
@@ -1,11 +1,6 @@
 require 'tensorflow/core/framework/tensor'
 require 'tensorflow/core/framework/graph'
-if ENV['rvm_version'].nil?
-  require 'tf/Tensorflow'
-else
-  require "#{ENV['GEM_HOME']}/gems/tensorflow-0.0.1/lib/tf/Tensorflow"
-end
-require 'tensor'
-require 'graph'
-require 'session'
 require 'sciruby/Tensorflow'
+require 'tensorflow/tensor'
+require 'tensorflow/graph'
+require 'tensorflow/session'
