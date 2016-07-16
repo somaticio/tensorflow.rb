2dad428a9bceda1153812f343851684ba88fcbf6
nethsix <nethsix@gmail.com>
2016-07-16 13:53:45 +0900

    Re-remove special conditions for handling rvm
    Previously removed code was accidentally re-introduced
    during some merge?
    Re-removing them again.
    
    NOTE: These code were added because previously, 'make install' was
    no successful because hidden file (.RUBYARCHDIR.-.tf.time) prevents
    creation of intermediary directories in target install directory.
    With the restructuring, and removing of unnecessary files in
    ext/, these code becomes unnecessary thus should remain deleted.

--- lib/tensorflow.rb
+++ lib/tensorflow.rb
@@ -1,10 +1,6 @@
 require 'tensorflow/core/framework/tensor'
 require 'tensorflow/core/framework/graph'
-if ENV['rvm_version'].nil?
-  require 'sciruby/Tensorflow'
-else
-  require "#{ENV['GEM_HOME']}/gems/tensorflow-0.0.1/lib/sciruby/Tensorflow"
-end
+require 'sciruby/Tensorflow'
 require 'tensorflow/tensor'
 require 'tensorflow/graph'
 require 'tensorflow/session'
