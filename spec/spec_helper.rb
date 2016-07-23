b45d52439be884f3a729076683c43ce1fa4a5555
Sebastian Deutsch <sebastian.deutsch@9elements.com>
2016-07-23 14:21:38 +0200

    Moved helper method to support and including all support in spec_helper.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,10 +2,6 @@ $LOAD_PATH.unshift File.expand_path('./../lib', __FILE__)
 require 'tensorflow'
 require 'pry'
 
-def load_and_extend_graph_from_file(filename)
-  session = Tensorflow::Session.new()
-  graph = Tensorflow::Graph.new()
-  graph.read(File.dirname(__FILE__)+'/example_graphs/'+filename)
-  session.extend_graph(graph)
-  session
-end
+# Requires supporting ruby files with custom matchers and macros, etc,
+# in spec/support/ and its subdirectories.
+Dir[File.dirname(__FILE__) + "/support/**/*.rb"].each {|f| require f }
