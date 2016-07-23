9a8e11f6afe15f0c94fc760de61fa73a576ee524
Arafatk <arafat@iitkgp.ac.in>
2016-07-23 22:12:50 +0530

    Removing redundant '/' and avoding repetition of tests.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,4 +4,4 @@ require 'pry'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
-Dir[File.dirname(__FILE__) + "/support/**/*.rb"].each {|f| require f }
+Dir[File.dirname(__FILE__) + "/support/*.rb"].each {|f| require f }
