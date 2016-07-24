cad84dc135b4bbcc0dc4e1f14f6d5ad31ccf5cd3
Arafatk <arafat@iitkgp.ac.in>
2016-07-25 02:20:01 +0530

    Adding Variable and constant Squashed commit of the following:
    commit 2c95c6bb82ee66d3953c71a314d008e12ed1bd0f
    Author: Arafatk <arafat@iitkgp.ac.in>
    Date:   Mon Jul 25 02:19:09 2016 +0530
    
        Adding Variable and constant
    
    commit 25482283cc053382170d239a3ad887626fdc7263
    Author: Arafatk <arafat@iitkgp.ac.in>
    Date:   Mon Jul 25 02:14:44 2016 +0530
    
        Adding Constant and Variable

---
+++ spec/constant_spec.rb
@@ -0,0 +1,32 @@
+require 'spec_helper'
+
+describe 'Constants' do
+  let(:graph) { Tensorflow::Graph.new }
+
+  let(:session) { Tensorflow::Session.new }
+  let(:result) { session.run(nil, ["output"], nil) }
+  before {
+    define_op
+    graph.graph_def_raw = Tensorflow::GraphDef.encode(graph.graph_def)
+    session.extend_graph(graph)
+  }
+
+  subject { result[0] }
+
+  context 'add' do
+    let(:input1) { graph.constant( "const1",[Complex(2,2), Complex(2,34)], :complex) }
+    let(:input2) { graph.constant( "const2",[Complex(2,2), Complex(-32,22)], :complex) }
+    let(:define_op) { graph.define_op("Add",'output', [input1, input2],"",nil) }
+
+    it { is_expected.to all_be_close([(4.0+4.0i), (-30.0+56.0i)]) }
+  end
+
+  context 'sub' do
+    # If we could use the same inputs for all tests, it would be even more DRY
+    let(:input1) { graph.constant( "const1",[634,432], :float64) }
+    let(:input2) { graph.constant( "const2",[332,332], :float64) }
+    let(:define_op) { graph.define_op("Sub",'output', [input1, input2],"",nil) }
+
+    it { is_expected.to all_be_close([302.0,100.0]) }
+  end
+end
