e50549285d32a8f2ac5955e4e2bf683bcda8b821
Arafatk <arafat@iitkgp.ac.in>
2016-07-26 21:12:41 +0530

    Solving load error

--- lib/tensorflow.rb
+++ lib/tensorflow.rb
@@ -1,6 +1,6 @@
 require 'tensorflow/core/framework/tensor'
 require 'tensorflow/core/framework/graph'
-require 'sciruby/Tensorflow'
+require "#{ENV['GEM_HOME']}/gems/tensorflow-0.0.1/lib/sciruby/Tensorflow"
 require 'narray'
 require 'tensorflow/tensor'
 require 'tensorflow/graph'
