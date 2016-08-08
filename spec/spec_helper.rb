1b7f08b38cb77def201dcb4df30a5ac18babff83
Arafat <arafat@iitkgp.ac.in>
2016-08-08 22:56:35 +0530

    Revert "Feature/coverage"

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,21 +1,6 @@
 $LOAD_PATH.unshift File.expand_path('./../lib', __FILE__)
-require 'simplecov'
-require 'simplecov-json'
-require 'simplecov-rcov'
-require 'pry'
-
-SimpleCov.formatters = [
-  SimpleCov::Formatter::HTMLFormatter,
-  SimpleCov::Formatter::JSONFormatter,
-  SimpleCov::Formatter::RcovFormatter
-]
-
-SimpleCov.start do
-  add_filter '/spec/'
-  add_group 'lib', 'lib'
-end
-
 require 'tensorflow'
+require 'pry'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
