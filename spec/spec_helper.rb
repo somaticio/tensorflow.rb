c898323028028f9b10189aa35371fd7dde7de414
Geoffrey Litt <gklitt@gmail.com>
2016-08-08 13:09:23 -0400

    Make name and datatype optional for Variables and Constants (#51)
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Make name and datatype optional for Variables and Constants
    
    The Python API allows name and datatype as optional parameters when
    creating a variable or a constant, making these declarations much
    more concise in typical cases. This changes the Ruby API to take
    those as optional parameters to more closely mirror the Python.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,3 +5,7 @@ require 'pry'
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
 Dir[File.dirname(__FILE__) + "/support/*.rb"].each {|f| require f }
+
+RSpec.configure do |c|
+  c.example_status_persistence_file_path = "examples.txt"
+end
--- spec/variable_spec.rb
+++ spec/variable_spec.rb
@@ -1,10 +1,49 @@
 require 'spec_helper'
 
 describe "Variable" do
-    it "Subtract Tensors" do
-    graph = Tensorflow::Graph.new
-    input1 = graph.variable("input1", [343,32], :int32)
-    input2 = graph.constant("input2", [33,42], :int32)
+  let(:graph) { Tensorflow::Graph.new }
+
+  it "sets the variable name when it is specified" do
+    no_name1 = graph.variable([1, 2, 3],  name: "testing_names")
+    expect(no_name1.ref.name).to eq("testing_names")
+  end
+
+  it "sets a default name if none is specified" do
+    no_name = graph.variable([1, 2, 3])
+    expect(no_name.ref.name).to eq("Variable:0")
+  end
+
+  it "increments the default variable name for each unnamed variable" do
+    no_name1 = graph.variable([1, 2, 3])
+    no_name2 = graph.variable([4, 5, 6])
+    expect(no_name1.ref.name).to eq("Variable:0")
+    expect(no_name2.ref.name).to eq("Variable:1")
+  end
+
+  it "increments variable and constant names separately" do
+    variable0 = graph.variable([0])
+    constant0 = graph.constant([0])
+    variable1 = graph.variable([1])
+    expect(variable0.ref.name).to eq("Variable:0")
+    expect(constant0.definition.name).to eq("Constant:0")
+    expect(variable1.ref.name).to eq("Variable:1")
+  end
+
+  it "sets data type when it is specified" do
+    no_type = graph.variable([1, 2, 3], dtype: :int32)
+    dtype = graph.type_to_enum(no_type.ref.attr["dtype"].type)
+    expect(dtype).to eq(Tensorflow::TF_INT32)
+  end
+
+  it "infers data type based on initial data if not explicitly specified" do
+    no_type = graph.variable([1, 2, 3])
+    dtype = graph.type_to_enum(no_type.ref.attr["dtype"].type)
+    expect(dtype).to eq(Tensorflow::TF_INT64)
+  end
+
+  it "subtracts tensors across multiple sessions" do
+    input1 = graph.variable([343,32], dtype: :int32, name: "input1")
+    input2 = graph.constant([33,42], dtype: :int32, name: "input2")
     add = graph.define_op("Sub",'add_tensors', [input1, input2],"",nil)
     graph.define_op("Assign",'assign_inp1', [input1, add],"",nil)
     graph.intialize_variables
@@ -16,4 +55,6 @@ describe "Variable" do
     result = session.run(nil, ["input1"], ["assign_inp1"])
     expect(result[0]).to match_array([244, -94])
   end
+
+
 end
