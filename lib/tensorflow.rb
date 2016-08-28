ff755790011d0db463911ae2767c573da0108047
Christian Hansen <christian@cobalt.io>
2016-08-27 20:11:24 -0700

    move shape-method to Tensor-class

--- lib/tensorflow.rb
+++ lib/tensorflow.rb
@@ -5,6 +5,3 @@ require 'narray'
 require 'tensorflow/tensor'
 require 'tensorflow/graph'
 require 'tensorflow/session'
-
-Dir[File.join(File.dirname(__FILE__), 'core_extensions', '**', '*.rb')]
-  .each { |file| require file }
