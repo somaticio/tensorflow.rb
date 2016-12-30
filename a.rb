add89846987a441a637706960740dec74317655b
Arafatk <arafat.da.khan@gmail.com>
2016-12-30 11:13:55 +0530

    Removing test protobuf files and ruby files.

--- a.rb
+++
@@ -1,8 +0,0 @@
-require 'tensorflow'
-file = File.open("a.pb", "rb")
-contents = file.read
-a = Tensorflow::Graph2.new
-# a.import(contents,"ttt")
-b = Tensorflow::Tensor.new([1,2,3])
-a.const("Car is okay", b)
-puts File.open('h.pb', 'w') { |file| file.write(a.writeto) }
