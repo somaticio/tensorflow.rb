c1129ac23c0b46cc97a967fa78c72e09866c57cb
Arafat <arafat@iitkgp.ac.in>
2017-03-12 21:17:18 +0530

    Update bazel version in install script

--- tools/install.sh
+++ tools/install.sh
@@ -26,7 +26,7 @@ PKG_CONFIG="http://pkgconfig.freedesktop.org/releases/pkg-config-0.28.tar.gz"
 
 # Bazel Source
 BAZEL_ROOT="https://github.com/bazelbuild/bazel/releases/download"
-BAZEL_VERS="0.3.1"
+BAZEL_VERS="0.4.4"
 PKG_BAZEL="$BAZEL_ROOT/$BAZEL_VERS/bazel-$BAZEL_VERS-installer-$platform-x86_64.sh"
 
 # TensorFlow source
